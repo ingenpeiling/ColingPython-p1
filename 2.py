72f7c55fcb3e84632f31f86008dd9a0bf235b8f2
ingenpeiling <ksenya_shev@yahoo.com>
2021-04-02 18:52:40 +0300

    FileReader issues corrected

--- 2.py
+++ 2.py
@@ -9,16 +9,15 @@ class FileReader:
     def __str__(self):
         return f'The path to your file is {self.path}'
     
-    def __add__(self, path_two, filename):     # filename - адрес (включающий имя) нового файла со склеенным текстом
-        with open(self.path, 'r') as file_one: # path_two - путь к второму текстовому файлу
-            text_one = file_one.read()
-        with open(path_two, 'r') as file_two:
-            text_two = file_two.read()
-        with open(filename, 'w') as new_file:
-            new_text = text_one + "\n" + text_two
-            new_file.write(new_text)
-        return self
-    
+    def __add__(self, other):
+        fnames = [self.path, other]
+        with open('new_file.txt', 'w') as new_file:
+            for fname in fnames:
+                with open(fname, 'r') as old_file:
+                    for line in old_file:
+                        new_file.write(line)
+        return new_file
+
     def read(self):
         try:
             with open(self.path, 'r') as file:
@@ -27,7 +26,7 @@ class FileReader:
         except FileNotFoundError:
             return ''
     
-    def write(self, text):                # если в файле уже что-то было, то старый текст исчезнет
+    def write(self, text):                          # если в файле уже что-то было, то старый текст исчезнет
         with open(self.path, 'w') as file:
             file.write(text)
         return self
@@ -35,11 +34,11 @@ class FileReader:
     def count(self):
         with open(self.path, 'r') as file:
             line_count = 0
+            word_count = 0
             for line in file:
+                len_line = len(word_tokenize(line))
                 line_count += 1
-        with open(self.path, 'r') as file:         
-            text = file.read()
-            word_count = len(word_tokenize(text))        
+                word_count = word_count + len_line   
         self.word_count = word_count
         self.line_count = line_count
         return word_count, line_count
