bb66a82144c894702bd05ea3a1d42c9686ac0800
ingenpeiling <ksenya_shev@yahoo.com>
2021-04-02 16:31:08 +0300

    Issues in HW2 corrected

--- HW2/G/G.py
+++ HW2/G/G.py
@@ -1,12 +1,13 @@
 def solution(a, b):
     initial_a = a.copy()
     for num_b in b:
-        if num_b not in initial_a:
-            before = 0
-            for num_a in a:
-                if num_b > num_a:
-                    before +=1
-                else:
-                    break  
-            a.insert(before, num_b)
+        if num_b in initial_a:
+            continue
+        before = 0
+        for num_a in a:
+            if num_b > num_a:
+                before +=1
+            else:
+                break  
+        a.insert(before, num_b)
     return a
